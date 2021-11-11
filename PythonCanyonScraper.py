9b676f3910ef5645feba997d15497f415dc6a8ac
Ben <34220038+newtob@users.noreply.github.com>
2021-11-11 14:09:26 +0000

    imported opentelemetry, did nothing else after long long gap

--- PythonCanyonScraper.py
+++ PythonCanyonScraper.py
@@ -1,5 +1,5 @@
 import datetime
-import os
+import os, sys
 from contextlib import closing
 
 from bs4 import BeautifulSoup
@@ -7,6 +7,12 @@ from google.cloud import bigquery
 from requests import get
 from requests.exceptions import RequestException
 from twilio.rest import Client
+
+from opentelemetry import trace
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace import Link
 
 
 def simple_get(url: str) -> str:
@@ -49,13 +55,51 @@ def parseSearch(raw_html: str) -> list:
         print("Type Error parsing raw html = " + e)
         exit(-1)
 
+    print('in parseSearch just before the html.select(span) bit')
+
+    #new routine
+    divCounterInt:Int = 0
+
+
+    html.find_all("div", class_="productTile__productSummaryLeft")
+
+    #TODO the above command SHOULD WORK. but does not. please, please make it work.
+
+
+    for s in html.find_all(itemtype='http://schema.org/Product'):
+        print('found something')
+        print(s)
+
+    for s in html.find_all('div', itemtype=True, recursive=True):
+        print(divCounterInt, end=',')
+        divCounterInt+=1
+
+        #print('d', end='')
+        #print(type(s))
+        #print(s.get('itemtype'))
+        #print(s['itemtype'])
+        print(s.attrs)
+        print(s)
+        if s.get('itemtype'):
+            print('found an itemtype')
+        #if s.get('class') is not None and "http://schema.org/Product" in s.get('itemtype'):
+        #    print('i found a schema//product')
+        #    print( s.data-pid )
+
+    sys.exit()
+
+    #old routine that doesn't parse the new page
     for s in html.select('span'):
         if s.get('class') is not None and 'productTile__productName' in s.get('class'):
+            print('found a productTile')
             bikeDataInfoList: list = []
             BikeName: str
             UID: str
             BikeName = s.text.replace("\\n", "").strip()
+            ## this next line is broken by the html. it's pulling ajax from above, incorrectly
+            ## doesn't work any more
             UID = str(s.previous_element.previous_element.previous_element['data-url']).split("=")[2]
+
             bikeDataInfoList.insert(0, UID)
             bikeDataInfoList.insert(1, BikeName)
 
@@ -75,37 +119,11 @@ def parseSearch(raw_html: str) -> list:
                     print("Error: Found either the orig price or sale price, but not both. Can't parse HTML correctly")
     return bikeDataListofLists
 
-def addGBPprices(bikeData: list) -> list:
-    """data struct starts of as a list of the following:[UID, BikeName, orig price, sale price, date found]
-    Then gbp_orig_price and gbp_sale_price and Percent_Discount is appended to the end"""
-    bikeDataAddition: list = bikeData
-    gbp_orig_price: int = 0
-    gbp_sale_price: int = 0
-    bike_date_list: list = []
-    newbike_list_of_list: list = []
-    bike_date_list: list = []
-
-    for i in bikeData:
-        bike_date_list.append(i)
-
-    for i, bike in enumerate(bike_date_list):
-        gbp_orig_price = (bike[2][1:-3].replace(',', ''))
-        gbp_sale_price = (bike[3][1:-3].replace(',', ''))
-        if "rom " in gbp_sale_price:
-            gbp_sale_price = gbp_sale_price[5:]
-        gbp_orig_price = int(gbp_orig_price)
-        gbp_sale_price = int(gbp_sale_price)
-
-        newbike_list_of_list.append([bike[0], bike[1], bike[2], bike[3], bike[4], gbp_orig_price, gbp_sale_price,
-                                     round((gbp_sale_price / gbp_orig_price) * 100)])
-    return newbike_list_of_list
-
 
 def checkBikeIsntLoadedAlready(bikeData: list, client: bigquery.client.Client) -> list:
     """Gets the UID's from the database and checks the newly scraped UID's, returning only the new ones"""
 
-    QUERY = (
-        'SELECT UID FROM `CanyonOutletBikeSaleData.CanyonOutletGBPBikeSaleDataTable` ')
+    QUERY = ('SELECT UID FROM `CanyonOutletBikeSaleData.CanyonOutletBikeSaleDataTable` ')
     query_job = client.query(QUERY)  # API request
     rows = query_job.result()  # Waits for query to finish
 
@@ -119,54 +137,41 @@ def checkBikeIsntLoadedAlready(bikeData: list, client: bigquery.client.Client) -
             UniqueBikestoAdd.append(individualBike)
     return UniqueBikestoAdd
 
-def check_bike_list(Bikelist: list) -> list:
-    """check structure is sound"""
-    brokenbikedata: list = []
-
-    for bike in Bikelist:
-        if type(bike[0]) is str and type(bike[1]) is str and type(bike[2]) is str and type(bike[3]) is str and type(bike[4]) is datetime.datetime:
-            pass
-        else:
-            brokenbikedata.append(bike)
-
-    return brokenbikedata
-
 
 def InsertintoDB(Bikelist: list, client: bigquery.client.Client) -> bool:
     """take a list of bike sales, output them into the DB
     Setup DB connection, for loop through insert rows
     """
 
-    table_id = "CanyonOutletBikeSaleData.CanyonOutletGBPBikeSaleDataTable"
+    table_id = "CanyonOutletBikeSaleData.CanyonOutletBikeSaleDataTable"
     table = client.get_table(table_id)  # Make an API request.
     rows_to_insert = Bikelist
-
-    checked_bike_list = check_bike_list(Bikelist)
-    if not len(checked_bike_list) == 0:
-        print("ERROR, scrape failed, the following bike failed : " + checked_bike_list)
 
     errors = client.insert_rows(table, rows_to_insert)  # Make an API request.
     if errors != []:
         print("ERROR: New rows have not been added, errors = " + str(errors))
         return False
     else:
-        print("bike rows successfully inserted into BQ = " + str(len(Bikelist)))
+        print("rows inserted = " + str(len(Bikelist)))
         return True
 
 
-# def env_vars():
-#     """for secrets and CICD"""
-#     # return os.environ.get('twilio_auth_token', 'Specified environment variable, twilio_auth_token, is not set.')
-#     return os.environ.get('twilio_auth_token', None)
+def env_vars():
+    """for secrets and CICD"""
+    # return os.environ.get('twilio_auth_token', 'Specified environment variable, twilio_auth_token, is not set.')
+    return os.environ.get('twilio_auth_token', None)
 
 
 def BikelisttoSMSAdvanced(bikelist: list) -> bool:
     """from this page: https://www.twilio.com/docs/sms/quickstart/python"""
 
+    # Your Account Sid and Auth Token from twilio.com/console
+    # DANGER! This is insecure. See http://twil.io/secure
     account_sid = os.environ.get('TWILIO_ACCOUNT_SID', None)
     auth_token = os.environ.get('twilio_auth_token', None)
+
     if auth_token is None:
-        print("auth token isn't present, check env variables")
+        print("auth token f*ed")
         exit(-1)
     SMSclient = Client(account_sid, auth_token)
     message: SMSclient
@@ -180,55 +185,51 @@ def BikelisttoSMSAdvanced(bikelist: list) -> bool:
                 body=messageData,
                 from_='+16506459228',
                 to='+447823772665')
-            print(message.sid)
+            # print(message.sid)
     return True
 
 
-def main(client: bigquery.Client, saveHTML: bool, test: bool = False) -> None:
+def main(client: bigquery.Client, test: bool, saveHTML: bool) -> None:
     """main method, checks to see if its an off line 'test' or if it needs to get data from the web.
     Saves a new set of html if it does go out to get it.
     Parses the output and should then save to cloud db."""
     if test:
-        with open('examplePageAeroadSizeM.html', 'r') as file:
+        print('im in test loop')
+        with open('latestAeroadSizeM2.html', 'r') as file:
             raw_html = str(file.read())
-        with open('examplePageAllBikes.html', 'r') as fileAll:
+        with open('latestAllBikes2.html', 'r') as fileAll:
             raw_max_html = str(fileAll.read())
     else:
-        raw_html = simple_get('https://www.canyon.com/en-gb/outlet/road-bikes/?cgid=outlet-road&\
-        prefn1=pc_familie&prefn2=pc_outlet&prefn3=pc_rahmengroesse&prefv1=Aeroad&prefv2=true&prefv3=M')
-        raw_max_html = simple_get('https://www.canyon.com/en-gb/outlet/road-bikes/?\
-        cgid=outlet-road&prefn1=pc_outlet&prefv1=true')
+        raw_html = simple_get('https://www.canyon.com/en-gb/outlet/road-bikes/?cgid=outlet-road&prefn1=pc_familie&prefn2=pc_rahmengroesse&prefv1=Aeroad&prefv2=M')
+        raw_max_html = simple_get('https://www.canyon.com/en-gb/outlet/road-bikes/')
         if raw_html == "none" or raw_max_html == "none":
             print("Critical error: no return from website")
             exit(-1)
         elif saveHTML:
-            with open("./latestAeroadSizeM.html", 'w') as out_file:
+            print('im going to save an html')
+            with open("./latestAeroadSizeM2.html", 'w', encoding="utf-8") as out_file:
                 out_file.writelines(raw_html)
-            with open("./latestAllBikes.html", 'w') as out_max_file:
+            with open("./latestAllBikes2.html", 'w', encoding="utf-8") as out_max_file:
                 out_max_file.writelines(raw_max_html)
 
     # Cycle for Aeroad Size M
-    AearoadBikelistToCheck = addGBPprices(parseSearch(raw_html))
+    AearoadBikelistToCheck = parseSearch(raw_html)
     BikelistToSMS = checkBikeIsntLoadedAlready(AearoadBikelistToCheck, client)
     if checkBikeIsntLoadedAlready:
         BikelisttoSMSAdvanced(BikelistToSMS)
 
     # Cycle for All bikes
-    BikelistToCheck = addGBPprices(parseSearch(raw_max_html))
+    BikelistToCheck = parseSearch(raw_max_html)
     BikelistToInsert = checkBikeIsntLoadedAlready(BikelistToCheck, client)
     if BikelistToInsert:
         InsertintoDB(BikelistToInsert, client)
 
 
-def __init__():
-    pass
-
-
 if __name__ == "__main__":
-    # test = True
-    test = False
-    myClient = bigquery.Client.from_service_account_json('./canyonscraper-ee35f3c9cec6.json')
-    main(myClient, test, False)
+    test = True
+    #test = False
+    myClient = bigquery.Client.from_service_account_json('./canyonscraper-54d54af48066.json')
+    main(myClient, test, True)
 
 
 def PythonCanyonScraper(event, context) -> None:
